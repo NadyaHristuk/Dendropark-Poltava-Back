a4e34d712c000eaddb69eaf908dbd0597247e212
Nadya Hristuk <elhe2013@gmail.com>
2024-04-08 15:00:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+
 
 app.use("/api/auth", authRouter);
 app.use("/api/events", eventsRouter);
@@ -31,13 +31,14 @@ app.use("/api/locations", locationsRouter);
 app.use("/api/donations", donationsRouter);
 app.use("/api/services", servicesRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+// Serve Static Files from React App
+app.use(express.static(path.join(__dirname, 'public')));
+
+// SPA Route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server Error" } = err;
-  res.status(status).json({ message });
-});
+
 
 module.exports = app;
